f60ea2a59f407f000d5ee47901461ce9c6e740a5
Ranjit T N <ranju0305@gmail.com>
2026-07-31 10:46:32 +0530

    module1 output images

--- Module1/question10/predictive_maintanence.py
+++ Module1/question10/predictive_maintanence.py
@@ -104,7 +104,7 @@ print("Target Shape :", y.shape)
 
 # Train-Test Split
 
-X_train, X_test, y_train, y_tes t = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
 print("\nTraining Samples :", len(X_train))
 print("Testing Samples :", len(X_test))
 
